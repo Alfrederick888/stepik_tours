fe59d31b6cfaa9bfa20c851bb61997be01a56638
Alfrederick888 <al-gilm@yandex.ru>
2023-02-28 18:51:28 +0500

    second_commit_tours

--- stepik_tours/stepik_tours/urls.py
+++ stepik_tours/stepik_tours/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tours.views import *
 
-from tours.views import MainView, DepartureView, TourView
 
 
 urlpatterns = [
     path('', MainView.as_view(), name='home'),
-    path('departure/', DepartureView.as_view(), name='departure'),
     path('tour/', TourView.as_view(), name='tour'),
     path('admin/', admin.site.urls),
+    path('departure/', DepartureView.as_view(), name='departure'),
+    path('spb/', SpbView.as_view(), name='spb'),
+    path('nsk/', NskView.as_view(), name='nsk'),
+    path('ekb/', EkbView.as_view(), name='ekb'),
+    path('kazan/', KazanView.as_view(), name='kazan'),
 ]
